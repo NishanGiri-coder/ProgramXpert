c676e02c2e311c82cb77575dca3a37eb72c3ba02
Nishan Giri <nishangiri95@gmail.com>
2025-06-09 22:02:09 +0545

    Add files via upload

---
+++ auth5.js
@@ -0,0 +1,113 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzpAddeNpJK0etyGByEgOhmXLRCEiD8vg",
+  authDomain: "programxperts.firebaseapp.com",
+  projectId: "programxperts",
+  storageBucket: "programxperts.firebasestorage.app",
+  messagingSenderId: "792221592009",
+  appId: "1:792221592009:web:ec20665c502a243077e018",
+  measurementId: "G-5R3NDCH5Q4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+// Show message function
+function showMessage(message, divId) {
+  var messageDiv = document.getElementById(divId);
+  messageDiv.style.display = "block";
+  messageDiv.innerHTML = message;
+  messageDiv.style.opacity = 1;
+  setTimeout(function () {
+    messageDiv.style.opacity = 0;
+  }, 5000);
+}
+
+// Sign up functionality
+const signUp = document.getElementById("submitSignUp");
+signUp.addEventListener("click", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("rEmail").value;
+  const password = document.getElementById("rPassword").value;
+  const firstName = document.getElementById("fName").value;
+  const lastName = document.getElementById("lName").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const userData = {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+      };
+      showMessage("Account Created Successfully", "signUpMessage");
+      const docRef = doc(db, "users", user.uid);
+      setDoc(docRef, userData)
+        .then(() => {
+          window.location.href = "form.html";
+        })
+        .catch((error) => {
+          console.error("Error writing document", error);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode === "auth/email-already-in-use") {
+        showMessage("Email Address Already Exists !!!", "signUpMessage");
+      } else {
+        showMessage("Unable to create User", "signUpMessage");
+      }
+    });
+});
+
+// Sign in functionality
+const signIn = document.getElementById("submitSignIn");
+signIn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      showMessage("Login is successful", "signInMessage");
+      const user = userCredential.user;
+      
+      localStorage.setItem("loggedInUserId", user.uid);
+      setTimeout(function () {
+        window.location.href = "python_intro.html"; // Redirect after 2 seconds
+      }, 1000);
+      
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode === "auth/invalid-credential") {
+        showMessage("Incorrect Email or Password", "signInMessage");
+      } else {
+        showMessage("Account does not Exist", "signInMessage");
+      }
+    });
+});
+
+//Toggle Sign In/Sign Up forms visibility
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signup');
+
+signUpButton.addEventListener('click', function () {
+  signInForm.style.display = "none";
+  signUpForm.style.display = "block";
+});
+signInButton.addEventListener('click', function () {
+  signInForm.style.display = "block";
+  signUpForm.style.display = "none";
+}); 
---
+++ search.js
@@ -0,0 +1,100 @@
+// Autocomplete function
+function autocomplete(inp, arr) {
+    var currentFocus;
+
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.addEventListener("click", function(e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+
+                    // Open the corresponding HTML page for the selected language
+                    const language = this.getElementsByTagName("input")[0].value.toLowerCase();
+                    window.open(`${language}.html`, "_blank");
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            currentFocus++;
+            addActive(x);
+        } else if (e.keyCode == 38) {
+            currentFocus--;
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            e.preventDefault();
+            if (currentFocus > -1) {
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(x) {
+        if (!x) return false;
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+// Array of programming languages
+var languages = ["C", "C++", "js", "Python"];
+
+// Initialize autocomplete function
+autocomplete(document.getElementById("myInput"), languages);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const messages = ["Hello", "Coders", "Start", "Your","Journey","Today!","Sharp","Your","Future..."];
+    let messageIndex = 0;
+  
+    function updateMessage() {
+      // Change the message
+      document.getElementById('dynamic-hello').textContent = messages[messageIndex];
+      
+      // Move to the next message
+      messageIndex = (messageIndex + 1) % messages.length;  // Loops back to 0 when it reaches the end of the array
+    }
+  
+    // Update the message every 3 seconds (3000ms)
+    setInterval(updateMessage, 3000);
+  });
+  
